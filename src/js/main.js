8dd83c33eb2f0b7c369eafcc761d9cdfd49e0c34
PabloChDAW <pcg0031@alu.medac.es>
2025-01-09 23:43:47 +0100

    Update main.js
    Lo último que falta antes del despliegue es comprobar que el JavaScript necesario se ejecute correctamente. En este momento, main.js importa el JavaScript de Bootstrap, pero luego no se utiliza.

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,3 @@ import '../scss/styles.scss'
 
 // Importa todo el JavaScript de Bootstrap
 import * as bootstrap from 'bootstrap'
-
-// Inicializa los componentes de Bootstrap cuando el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', () => {
-  // Inicializar todos los carruseles
-  const carouselElements = document.querySelectorAll('.carousel')
-  carouselElements.forEach(carouselElement => {
-    new bootstrap.Carousel(carouselElement, {
-      interval: 2000, // Cambia el intervalo de tiempo entre slides (en milisegundos)
-      ride: 'carousel' // Inicia el carrusel automáticamente
-    })
-  })
-
-  // Inicializar todas las ventanas modales
-  const modalElements = document.querySelectorAll('.modal')
-  modalElements.forEach(modalElement => {
-    new bootstrap.Modal(modalElement)
-  })
-})
